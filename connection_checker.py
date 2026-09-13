3c3598c6bfa69bfe11b0330a64ca5851f8a6c43c
bin-65 <usmanbinsaeed625@gmail.com>
2026-09-13 23:25:04 +0500

    Update connection_checker.py

--- connection_checker.py
+++ connection_checker.py
@@ -1,12 +1,9 @@
 import socket
 
-def check_internet_connection(host="8.8.8.8", port=53, timeout=3):
-    """
-    Checks if internet connection is available by connecting to Google's DNS server.
-    """
+def check_internet_connection():
     try:
-        socket.setdefaulttimeout(timeout)
-        socket.socket(socket.AF_INET, socket.SOCK_STREAM).connect((host, port))
+        # Check if internet/Google DNS is reachable
+        socket.create_connection(("8.8.8.8", 53), timeout=3)
         return True
-    except Exception:
+    except OSError:
         return False
