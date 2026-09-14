b2214b93c8b8bb3d647053a769bbe4e8d9c6e6b5
bin-65 <usmanbinsaeed625@gmail.com>
2026-09-14 15:49:16 +0500

    Update connection_checker.py

--- connection_checker.py
+++ connection_checker.py
@@ -1,12 +1,1 @@
-import socket
 
-def is_connected(host="8.8.8.8", port=53, timeout=3):
-    """
-    Checks if there is an active internet connection by pinging Google DNS.
-    """
-    try:
-        socket.setdefaulttimeout(timeout)
-        socket.socket(socket.AF_INET, socket.SOCK_STREAM).connect((host, port))
-        return True
-    except Exception:
-        return False
